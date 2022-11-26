e2aa0144e6e45553d144f9a1d59f586333f387ac
Victor Hugo <victorhugo_mr@live.com>
2022-11-26 17:15:23 -0300

    Update 1040 - Average 3.cs

--- C#/Beecrowd/1040 - Average 3.cs
+++ C#/Beecrowd/1040 - Average 3.cs
@@ -3,8 +3,9 @@ using System;
 namespace Beecrowd{
 
 	class URI {
+	    
 		static void Main(string[] args){ 
-			
+
 			string[] scanf = Console.ReadLine().Split();
 			double n1 = double.Parse(scanf[0]);
 			double n2 = double.Parse(scanf[1]);
@@ -13,34 +14,34 @@ namespace Beecrowd{
 			double media = Math.Floor((n1*2 + n2*3 + n3*4 + n4*1)/(2+3+4+1) *100)/100;
 			double notaExame = 0;
 			double mediaFinal = 0;
-    
-    	Console.WriteLine($"Media: {media:0.0}");
-        
-      if(media>=7){
-    	  Console.WriteLine($"Aluno aprovado.");
-      }
-      else{
-        if(media<=4.9){
-          Console.WriteLine($"Aluno reprovado.");
-        }
-        else{
-          Console.WriteLine($"Aluno em exame.");
-        	
-          notaExame = double.Parse(Console.ReadLine());
-          
-          Console.WriteLine($"Nota do exame: {notaExame:0.0}");
-        			
-          mediaFinal = (media + notaExame)/2;
-        			
-        	if(mediaFinal<=4.9){
-            Console.WriteLine($"Aluno reprovado.");
-          }
-          else{
-            Console.WriteLine($"Aluno aprovado.");
-            Console.WriteLine($"Media final: {mediaFinal}");
-          }
-        }
-      }
-	  }
+
+			Console.WriteLine($"Media: {media:0.0}");
+
+			if(media>=7){
+				Console.WriteLine($"Aluno aprovado.");
+			}
+			else{
+				if(media<=4.9){
+					Console.WriteLine($"Aluno reprovado.");
+				}
+				else{
+					Console.WriteLine($"Aluno em exame.");
+
+					notaExame = double.Parse(Console.ReadLine());
+
+					Console.WriteLine($"Nota do exame: {notaExame:0.0}");
+
+					mediaFinal = (media + notaExame)/2;
+
+					if(mediaFinal<=4.9){
+						Console.WriteLine($"Aluno reprovado.");
+					}
+					else{
+						Console.WriteLine($"Aluno aprovado.");
+						Console.WriteLine($"Media final: {mediaFinal:0.0}");
+					}
+				}
+			}
+		}
 	}
 }
