94c3df07b2f1a5c0c2e6bdda08f8b43daa535c75
Victor Hugo <victorhugo_mr@live.com>
2022-11-26 18:43:17 -0300

    Create  1042 - Simple Sort.cs

---
+++ C#/Beecrowd/1042 - Simple Sort.cs
@@ -0,0 +1,15 @@
+using System; 
+
+namespace Beecrowd{
+
+    class URI {
+    
+        static void Main(string[] args) { 
+    
+			      string[] scanf = Console.ReadLine().Split();
+            
+            Console.WriteLine("\n{0}\n{1}\n{2}\n", scanf);
+            
+        }
+    }
+}
