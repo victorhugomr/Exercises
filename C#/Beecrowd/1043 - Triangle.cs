8a38d64bc9bdccb5475172884b56b6bce0fc1032
Victor Hugo <victorhugo_mr@live.com>
2022-11-28 22:31:32 -0300

    Update 1043 - Triangle.cs

--- C#/Beecrowd/1043 - Triangle.cs
+++ C#/Beecrowd/1043 - Triangle.cs
@@ -5,7 +5,7 @@ namespace Beecrowd{
 
     public class Program{
         
-		bool checkValidity(int a, int b, int c){
+		static bool checkValidity(double a, double b, double c){
 			if (a + b <= c || a + c <= b || b + c <= a){
 				return false;
 			}
@@ -16,15 +16,15 @@ namespace Beecrowd{
 		
     	public static void Main(){
     		
-    		double[] scanf = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
-			double triangulo = 0;
-			double trapezio = 0;
+    		double[] scanf = Console.ReadLine().Split().Select(double.Parse).ToArray();
+			double triangulo = scanf[0] + scanf[1] + scanf[2];
+			double trapezio = ((scanf[0]+scanf[1]) * scanf[2])/2;
     		
-    		if(checkValidity(a, b, c)){
-				Console.WriteLine("Perimetro = {triangulo}");
+    		if(checkValidity(scanf[0], scanf[1], scanf[2])){
+				Console.WriteLine($"Perimetro = {triangulo:0.0}");
 			}
 			else{
-				Console.WriteLine("Area = {trapezio}");
+				Console.WriteLine($"Area = {trapezio:0.0}");
 			}
     	}
     }
