552ca8cb9f74be0dfcc492dd07d1229d360f15e2
Victor Hugo <victorhugo_mr@live.com>
2022-11-29 17:21:23 -0300

    Create  1059 - Even Numbers.cs

---
+++ C#/Beecrowd/1059 - Even Numbers.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Beecrowd{
+
+    public class Program{
+        
+    	public static void Main(){
+    		
+			for(int i=1; i<=100; i++){
+				if(i%2==0)
+					Console.WriteLine(i);
+			}
+    	}
+    }
+}
