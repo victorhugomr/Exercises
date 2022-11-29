6c03f8d8bb7400360ca6491e05dadc67cc8db4f8
Victor Hugo <victorhugo_mr@live.com>
2022-11-29 19:53:24 -0300

    Create  1064 - Positives and Average.cs

---
+++ C#/Beecrowd/1064 - Positives and Average.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+
+namespace Beecrowd{
+
+    public class Program{
+        
+        static int checkLength(double[] numbers){
+            int tamanho = 0;
+            
+			for(int i=0; i<numbers.Length; i++){
+				numbers[i] = double.Parse(Console.ReadLine());
+				if(numbers[i]>0)
+				    tamanho += 1;
+			}
+			return tamanho;
+		}
+        
+    	public static void Main(){
+    		
+    		double[] numbers = new double[6];
+			double[] positiveNumbers = new double[checkLength(numbers)];
+			double average = 0;
+			int j=0;
+			
+			for(int i=0; i<numbers.Length; i++){
+				if(numbers[i]>0){
+				    positiveNumbers[j] = numbers[i];
+				    j++;
+				}
+			}
+			
+			for(int i=0; i<positiveNumbers.Length; i++){
+				average = average + positiveNumbers[i];
+			}
+			
+			average = average/positiveNumbers.Length;
+			
+			Console.WriteLine($"{positiveNumbers.Length} valores positivos");
+			Console.WriteLine($"{average:0.0}");
+    	}
+    }
+}
